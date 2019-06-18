481ef26a5b11f334b375522818a60e3e772b461a
sabina akter <33559730+aktersabina122@users.noreply.github.com>
2019-06-18 00:00:28 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+import ZipResult from './ZipResult';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props); 
+
+    this.state = {
+      zipcodes: []
+    }
+  }
+
+  searchZip = ( event ) => {
+    event.preventDefault();
+    let userInput = event.target[0].value;
+    console.log(userInput);
+
+    axios.get('http://ctp-zip-api.herokuapp.com/zip/'+ userInput)
+    .then( (resolve) => {   
+    // Calling when data is returned
+      this.setState( {zipcodes: resolve.data} );
+      console.log(resolve.data);
+    })
+    .catch( (err)=> console.log(err));
+  }
+
+  render() {
+    let elems = [];     
+    elems = this.state.zipcodes.map((elem, i) => <ZipResult key={i} {...elem} />)
+    return (
+      <div> 
+        <form onSubmit={this.searchZip}>  
+          <h1>Home</h1>
+          <input type="text" />
+        </form>
+
+        {elems}
+      </div>
+    );
+  }
+}
+
+export default App;
